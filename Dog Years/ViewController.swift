3c3aaa21f046c54c674ce147e3876f07b4d2cbc5
David Grevink <dgrevink@starplace.org>
2014-11-09 18:00:06 -0500

    Initial import.

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearTextField: UITextField!
+    @IBOutlet weak var humanYearLabel: UITextField!
+    let conversionConstant = 7;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertDogYearsToHumanYearsButton(sender: AnyObject) {
+        var dogYears : Int? = dogYearTextField.text.toInt();
+        
+        if (dogYears == nil) {
+            humanYearLabel.text = "Error ! Please enter an integer"
+            humanYearLabel.textColor = UIColor.redColor()
+        }
+        else {
+            var humanYears = dogYears! * conversionConstant;
+            humanYearLabel.text = "In human years, your dog is \(humanYears) years old.";
+            humanYearLabel.textColor = UIColor.brownColor()
+        }
+        
+        
+    }
 
 }
 
